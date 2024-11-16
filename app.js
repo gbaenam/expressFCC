749d3892214ab476ac0379450bcf0ce4c6bce9ad
German Baena <germanb66@gmail.com>
2024-11-15 19:11:25 -0500

    Se completaron los parámetros dinamicos

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express()
 
 const { infoCursos } = require('./cursos.js')
 
+
+// Routing general
+
 app.get('/', (req, res) => {
     res.send('Mi primer servidor con Express.')
 })
@@ -13,12 +16,41 @@ app.get('/api/cursos', (req, res) => {
     res.send(JSON.stringify(infoCursos))
 })
 
+
+
+// Routing programación
+
 app.get('/api/cursos/programacion', (req, res) => {
     res.send(JSON.stringify(infoCursos.programacion))
 })
 
+app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
+    const lenguaje = req.params.lenguaje
+    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje)
+
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`)
+    }
+
+    res.send(JSON.stringify(resultados))
+})
+
+
+// Routing matemáticas
+
 app.get('/api/cursos/matematicas', (req, res) => {
     res.send(JSON.stringify(infoCursos.matematicas))
+})
+
+app.get('/api/cursos/matematicas/:tema', (req, res) => {
+    const tema = req.params.tema
+    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema)
+
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${tema}`)
+    }
+
+    res.send(JSON.stringify(resultados))
 })
 
 const PUERTO = process.env.PORT || 3000
