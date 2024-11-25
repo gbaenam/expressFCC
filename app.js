07dee2946589a8bd8c2435f7f6ff80285f9960a7
German Baena <germanb66@gmail.com>
2024-11-25 17:21:54 -0500

    Se separó exitosamente el código de cada router en su carpeta correspondiente

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const express = require('express')
 
 const app = express()
 
-const { infoCursos } = require('./cursos.js')
+const { infoCursos } = require('./datos/cursos.js')
 
 
 // Routers
 
-const routerProgramacion = express.Router()
+const routerProgramacion = require('./routers/programacion.js')
 app.use('/api/cursos/programacion', routerProgramacion)
 
-const routerMatematicas = express.Router()
+const routerMatematicas = require('./routers/matematicas.js')
 app.use('/api/cursos/matematicas', routerMatematicas)
+
 
 
 // Routing general
@@ -26,65 +27,11 @@ app.get('/api/cursos', (req, res) => {
 })
 
 
-
-// Routing programación
-
-routerProgramacion.get('/', (req, res) => {
-    res.send(JSON.stringify(infoCursos.programacion))
-})
-
-routerProgramacion.get('/:lenguaje', (req, res) => {
-    const lenguaje = req.params.lenguaje
-    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje)
-
-    if (resultados.length === 0) {
-        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`)
-    }
-
-    // Parámetros query
-    if (req.query.ordenar === 'vistas') {
-        return res.send(JSON.stringify(resultados.sort((a, b) => b.vistas - a.vistas)))
-    }
-
-    res.send(JSON.stringify(resultados))
-})
-
-routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
-    const lenguaje = req.params.lenguaje
-    const nivel = req.params.nivel
-    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel)
-
-    if (resultados.length === 0) {
-        return res.status(404).send(`No se encontraron cursos de ${lenguaje}, nivel ${nivel}`)
-    }
-
-    res.send(JSON.stringify(resultados))
-})
-
-
-// Routing matemáticas
-
-routerMatematicas.get('/', (req, res) => {
-    res.send(JSON.stringify(infoCursos.matematicas))
-})
-
-routerMatematicas.get('/:tema', (req, res) => {
-    const tema = req.params.tema
-    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema)
-
-    if (resultados.length === 0) {
-        return res.status(404).send(`No se encontraron cursos de ${tema}`)
-    }
-
-    res.send(JSON.stringify(resultados))
-})
-
 const PUERTO = process.env.PORT || 3000
 
 app.listen(PUERTO, () => {
     console.log(`El servidor está escuchando en el puerto ${PUERTO}`)
 })
-
 
 
 
